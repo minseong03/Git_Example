03d644e5372e95fb4612cd609baba8eb6b513ffd
minseong03 <ii4gsp@naver.com>
2019-07-22 13:07:07 +0900

    First Python

---
+++ day1.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("Hello", "World", sep="=") # sep
+print("010", "1234", "5678", sep="-") # sep
+
+print("Hello")
+print("World")
+print("Hello", end="-") # end
+print("World")
+
+#실습 안녕!하세요. 친구들
+print("안녕", "하세요", sep="!", end=".")
+print("친구들")
